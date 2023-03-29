be9c56c5cbee3e916f4ef5c0ece158a1bd72d849
Федоренко Володимир <volodymyr.fedorenko@nure.ua>
2023-03-29 20:19:56 +0300

    Add connection to TestDb, QuantEdDbContext class and configure Program file

---
+++ Backend/DL/QuantEdDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DL
+{
+    public class QuantEdDbContext : DbContext
+    {
+        public QuantEdDbContext(DbContextOptions<QuantEdDbContext> options) : base(options)
+        {
+
+        }
+
+
+        //public DbSet<Course> Courses { get; set; }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Att>()
+        //        .HasOne(a => a.Test)
+        //        .WithMany(t => t.Atts)
+        //        .OnDelete(DeleteBehavior.Restrict);
+        //}
+    }
+}
--- Backend/WebAPI/Program.cs
+++ Backend/WebAPI/Program.cs
@@ -1,8 +1,16 @@
+using DL;
+using Microsoft.EntityFrameworkCore;
+//using MySql.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string connectionString = builder.Configuration.GetConnectionString("TestDB:MySql");
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<QuantEdDbContext>(options => options.UseMySQL(connectionString));
+
+// Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
