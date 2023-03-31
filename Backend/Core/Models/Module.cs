c4e574f513bba07fea8cde99d4a1835b400b679e
Федоренко Володимир <volodymyr.fedorenko@nure.ua>
2023-03-31 17:07:11 +0300

    Create classes Course, Module, Content Container and enum Topic

---
+++ Backend/Core/Models/Module.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models;
+
+public class Module
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime Estimate { get; set; }
+}
