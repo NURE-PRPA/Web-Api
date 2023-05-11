d58a338e6a0133d53d0597aa5037349c3d95d772
Федоренко Володимир <volodymyr.fedorenko@nure.ua>
2023-05-11 14:06:26 +0300

    Add AbstractUser logic

--- Backend/Core/Models/Abstractions/AbstractUser.cs
+++ Backend/Core/Models/Abstractions/AbstractUser.cs
@@ -7,5 +7,13 @@ namespace Core.Models;
 
 public abstract class AbstractUser
 {
-    
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool Gender { get; set; }
+    public DateOnly DOB { get; set; }
+    public string Email { get; set; }
+    public DateTime Date { get; set; }
+    public long GoogleId { get; set; }
+    public string Password { get; set; }
 }
--- Backend/Core/Models/Listener.cs
+++ Backend/Core/Models/Listener.cs
@@ -8,15 +8,6 @@ namespace Core.Models;
 
 public class Listener : AbstractUser
 {
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public bool Gender { get; set; }
-    public DateOnly DOB { get; set; }
-    public string Email { get; set; }
-    public DateTime Date { get; set; }
-    public long GoogleId { get; set; }
-    public string Password { get; set; }
     public byte BanCount { get; set; }
     public List<Ban> Bans { get; set; }
     public List<Subscription> Subscriptions { get; set; }
